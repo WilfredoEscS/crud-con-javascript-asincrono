806cc36dd6ca80a9f0d0950e606f441145b9b33d
WilfredoEscS <wilfredosorto75@gmail.com>
2022-12-25 15:04:36 -0600

    Muestra la lista de clientes
    Que se encuentran registrados en el servidor

---
+++ services/client-services.js
@@ -0,0 +1,47 @@
+const crearNuevaLinea = (nombre, email) => {
+  const linea = document.createElement("tr");
+  const contenido = `
+    <td class="td" data-td>
+      ${nombre}
+    </td>
+    <td>${email}</td>
+    <td>
+      <ul class="table__button-control">
+        <li>
+          <a
+            href="../screens/editar_cliente.html"
+            class="simple-button simple-button--edit"
+          >
+            Editar
+          </a>
+        </li>
+        <li>
+          <button class="simple-button simple-button--delete" type="button">
+            Eliminar
+          </button>
+        </li>
+      </ul>
+    </td>`;
+  linea.innerHTML = contenido;
+  return linea;
+};
+
+const table = document.querySelector("[data-table]");
+const http = new XMLHttpRequest();
+
+// inicializa una solicitud recién creada o reinicializa una existente.
+//recibe un metodo y una url
+http.open("GET", "http://localhost:3000/perfil");
+
+//envía la solicitud al servidor.
+http.send();
+
+//Ejecuta la funcion flecha cuando http se  ha cargado
+http.onload = () => {
+  const data = JSON.parse(http.response);
+  data.forEach((perfil) => {
+    const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+    table.appendChild(nuevaLinea);
+  });
+  console.log(data);
+};
